3ad7076e9cb79b1d59eff9cab49d3244c422840a
CSN <CSN@CNS7188>
2016-09-28 17:40:27 +0900

    v

--- src/com/dto/PetDTO.java
+++ src/com/dto/PetDTO.java
@@ -2,6 +2,7 @@ package com.dto;
 
 public class PetDTO {
 	private int m_num;
+	private String userid;
 	private String p_kkcnumber;
 	private String p_name;
 	private int p_age;
@@ -11,12 +12,40 @@ public class PetDTO {
 	private String p_photo;
 	private String p_feature;
 
+	public PetDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public PetDTO(int m_num, String userid, String p_kkcnumber, String p_name, int p_age, String p_gender,
+			String p_birth, String p_type, String p_photo, String p_feature) {
+		super();
+		this.m_num = m_num;
+		this.userid = userid;
+		this.p_kkcnumber = p_kkcnumber;
+		this.p_name = p_name;
+		this.p_age = p_age;
+		this.p_gender = p_gender;
+		this.p_birth = p_birth;
+		this.p_type = p_type;
+		this.p_photo = p_photo;
+		this.p_feature = p_feature;
+	}
+
 	public int getM_num() {
 		return m_num;
 	}
 
 	public void setM_num(int m_num) {
 		this.m_num = m_num;
+	}
+
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
 	}
 
 	public String getP_kkcnumber() {
@@ -85,28 +114,9 @@ public class PetDTO {
 
 	@Override
 	public String toString() {
-		return "PetDTO [m_num=" + m_num + ", p_kkcnumber=" + p_kkcnumber + ", p_name=" + p_name + ", p_age=" + p_age
-				+ ", p_gender=" + p_gender + ", p_birth=" + p_birth + ", p_type=" + p_type + ", p_photo=" + p_photo
-				+ ", p_feature=" + p_feature + "]";
-	}
-
-	public PetDTO(int m_num, String p_kkcnumber, String p_name, int p_age, String p_gender, String p_birth,
-			String p_type, String p_photo, String p_feature) {
-		super();
-		this.m_num = m_num;
-		this.p_kkcnumber = p_kkcnumber;
-		this.p_name = p_name;
-		this.p_age = p_age;
-		this.p_gender = p_gender;
-		this.p_birth = p_birth;
-		this.p_type = p_type;
-		this.p_photo = p_photo;
-		this.p_feature = p_feature;
-	}
-
-	public PetDTO() {
-		super();
-		// TODO Auto-generated constructor stub
+		return "PetDTO [m_num=" + m_num + ", userid=" + userid + ", p_kkcnumber=" + p_kkcnumber + ", p_name=" + p_name
+				+ ", p_age=" + p_age + ", p_gender=" + p_gender + ", p_birth=" + p_birth + ", p_type=" + p_type
+				+ ", p_photo=" + p_photo + ", p_feature=" + p_feature + "]";
 	}
 
 }
